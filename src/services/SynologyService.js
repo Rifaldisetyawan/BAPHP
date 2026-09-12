1564646fb453a23b301e731cb6b7aa074d920091
Admin <rifaldisetyawan21@gmail.com>
2026-09-12 11:40:20 +0700

    ganti base url. sebelumnya /api.php

--- src/services/SynologyService.js
+++ src/services/SynologyService.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://baphp.dinaspu.com/'
+const BASE_URL = 'https://baphp.dinaspu.com/api.php'
 
 export const uploadToSynologyLocal = async (file, jobName = '') => {
   const formData = new FormData()
